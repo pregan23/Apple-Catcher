8ca2c5e5129e3089733bf6e99c168018f43c8249
pregan23 <pregan2bing@gmail.com>
2022-03-07 15:07:26 -0500

    animated red box falling

--- app.js
+++ app.js
@@ -3,16 +3,29 @@ const playArea = document.querySelector('.play-area')
 
 //gonna need a function to spawn apples and another one to make them move
 //spawn apples as span elements?
+const moveApple = () => {
+   let id = null 
+   const apple = document.querySelector('.Apple')
+   let pos = 0
+   clearInterval(id)
+   id = setInterval(frame, 10);
+   function frame()  {
+       if (pos === 350) {
+           clearInterval(id)
+       }
+       else {
+           pos++;
+           apple.style.top = pos + 'px'
+       }
+   }
+   
+}
 
 const spawnApple = () => {
     const newApple = document.createElement('span')
     newApple.classList.add('Apple')
     playArea.appendChild(newApple)
+
 }
 spawnApple()
-spawnApple()
-spawnApple()
-spawnApple()
-spawnApple()
-spawnApple()
-spawnApple()
+moveApple()
