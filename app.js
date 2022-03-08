2cd6f7d7f9b136b74153cb737a97fb846a0fa9a2
pregan23 <pregan2bing@gmail.com>
2022-03-07 20:01:08 -0500

    3 apples all fall

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 let score = 0
-// let appNum = 1
+let appNum = 1
 const playArea = document.querySelector('.play-area')
 
 
 //gonna need a function to spawn apples and another one to make them move
 //spawn apples as span elements?
-const moveApple = () => {
-    let id = null 
-    const apple = document.querySelector('.Apple')
+const moveApple = (appNum) => {
+    let id = null
+    // let currentApple = `#Apple${appNum}`
+    let apple = document.querySelector('#Apple'+CSS.escape(appNum))
+    console.log(apple)
+    console.log(appNum)
     let pos = 0
     clearInterval(id)
     id = setInterval(frame, 5);
@@ -20,19 +23,36 @@ const moveApple = () => {
             apple.style.top = pos + 'px'
         }
     }
+
+    
     
  }
  
  const spawnApple = () => {
-     const horVal = Math.random() * (200-1);
+     const horVal = Math.random() * (800-1);
      const newApple = document.createElement('span')
      newApple.classList.add('Apple')
-     newApple.style.left = horVal + '%'
+     newApple.id='Apple'+appNum
+     newApple.style.left = horVal + 'px'
      playArea.appendChild(newApple)
+     
+
+     moveApple(appNum)
  
  }
+ 
  spawnApple()
- moveApple()
+ appNum+=1
+ spawnApple()
+ appNum+=1
+ spawnApple()
+//  for(let i = 0; i<3;i++) {
+//      spawnApple()
+//      moveApple(i)
+//  }
+ 
+ 
+//  moveApple()
 
 
     // appNum+=1
